7aa81628c00fde28895c9d29e2c466f0e08d50da
Savvas Dalkitsis <kurosavvas@gmail.com>
2021-03-16 00:37:59 +0000

    Adding version file

--- kevin-nation.bat
+++ kevin-nation.bat
@@ -1,8 +1,14 @@
 set app=%APPDATA%\kevin-nation-modpack
+set instance=%app%\instance
 set rclone-version=v1.54.1
 set rclone-zip=%app%\rclone-%rclone-version%.zip
+set modpack-version=v0.0.1
+set conf=%app%\conf
+set rclone-config=%app%\conf\rclone-%modpack-version%.conf
 
 if not exist %app% mkdir %app%
+if not exist %conf% mkdir %conf%
+if not exist %instance% mkdir %instace%
 
 IF not EXIST %rclone-zip% (
     bitsadmin.exe /transfer Job "https://downloads.rclone.org/%rclone-version%/rclone-%rclone-version%-windows-amd64.zip" %rclone-zip%
@@ -10,5 +16,9 @@ IF not EXIST %rclone-zip% (
 
 if not EXIST %app%/rclone-%rclone-version%-windows-amd64/rclone.exe (
     tar -xf %rclone-zip% -C %app%
+)
+
+IF not EXIST %rclone-config% (
+    bitsadmin.exe /transfer Job "https://raw.githubusercontent.com/savvasdalkitsis/minecraft-modpack-kevin-nation/%modpack-version%/rclone.conf" %rclone-config%
 )
 
