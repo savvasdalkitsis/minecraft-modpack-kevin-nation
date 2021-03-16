2fdd1a71d443094b74a28277bee3ab64704ce229
Savvas Dalkitsis <kurosavvas@gmail.com>
2021-03-16 00:44:23 +0000

    Creating main script which always downloads the latest version

--- kevin-nation.bat
+++ kevin-nation.bat
@@ -1,24 +1,7 @@
 set app=%APPDATA%\kevin-nation-modpack
-set instance=%app%\instance
-set rclone-version=v1.54.1
-set rclone-zip=%app%\rclone-%rclone-version%.zip
-set modpack-version=v0.0.1
-set conf=%app%\conf
-set rclone-config=%app%\conf\rclone-%modpack-version%.conf
 
 if not exist %app% mkdir %app%
-if not exist %conf% mkdir %conf%
-if not exist %instance% mkdir %instace%
 
-IF not EXIST %rclone-zip% (
-    bitsadmin.exe /transfer Job "https://downloads.rclone.org/%rclone-version%/rclone-%rclone-version%-windows-amd64.zip" %rclone-zip%
-)
+bitsadmin.exe /transfer Job "https://raw.githubusercontent.com/savvasdalkitsis/minecraft-modpack-kevin-nation/main/process.bat" %app%\process.bat
 
-if not EXIST %app%/rclone-%rclone-version%-windows-amd64/rclone.exe (
-    tar -xf %rclone-zip% -C %app%
-)
-
-IF not EXIST %rclone-config% (
-    bitsadmin.exe /transfer Job "https://raw.githubusercontent.com/savvasdalkitsis/minecraft-modpack-kevin-nation/%modpack-version%/rclone.conf" %rclone-config%
-)
-
+CALL %app%\process.bat
