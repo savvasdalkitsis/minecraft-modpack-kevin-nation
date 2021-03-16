9fab9bef469e29c1a15e038cbe311cff1fc5d97a
Savvas Dalkitsis <kurosavvas@gmail.com>
2021-03-16 16:46:08 +0000

    Naming jobs and versioning rclone config separately

--- kevin-nation.bat
+++ kevin-nation.bat
@@ -2,6 +2,6 @@ set app=%APPDATA%\kevin-nation-modpack
 
 if not exist %app% mkdir %app%
 
-bitsadmin.exe /transfer Job "https://raw.githubusercontent.com/savvasdalkitsis/minecraft-modpack-kevin-nation/main/process.bat" %app%\process.bat
+bitsadmin.exe /transfer DownloadingLatestScript "https://raw.githubusercontent.com/savvasdalkitsis/minecraft-modpack-kevin-nation/main/process.bat" %app%\process.bat
 
 CALL %app%\process.bat
--- process.bat
+++ process.bat
@@ -4,14 +4,15 @@ set rclone-version=v1.54.1
 set rclone-zip=%app%\rclone-%rclone-version%.zip
 set modpack-version=v0.0.1
 set conf=%app%\conf
-set rclone-config=%app%\conf\rclone-%modpack-version%.conf
+set rclone-config-version=1.0.0
+set rclone-config=%app%\conf\rclone-%rclone-config-version%.conf
 
 if not exist %app% mkdir %app%
 if not exist %conf% mkdir %conf%
 if not exist %instance% mkdir %instace%
 
 IF not EXIST %rclone-zip% (
-    bitsadmin.exe /transfer Job "https://downloads.rclone.org/%rclone-version%/rclone-%rclone-version%-windows-amd64.zip" %rclone-zip%
+    bitsadmin.exe /transfer DownloadingRclone "https://downloads.rclone.org/%rclone-version%/rclone-%rclone-version%-windows-amd64.zip" %rclone-zip%
 )
 
 if not EXIST %app%/rclone-%rclone-version%-windows-amd64/rclone.exe (
@@ -19,6 +20,6 @@ if not EXIST %app%/rclone-%rclone-version%-windows-amd64/rclone.exe (
 )
 
 IF not EXIST %rclone-config% (
-    bitsadmin.exe /transfer Job "https://raw.githubusercontent.com/savvasdalkitsis/minecraft-modpack-kevin-nation/%modpack-version%/rclone.conf" %rclone-config%
+    bitsadmin.exe /transfer DownloadingRcloneConfig "https://raw.githubusercontent.com/savvasdalkitsis/minecraft-modpack-kevin-nation/%modpack-version%/rclone.conf" %rclone-config%
 )
 
